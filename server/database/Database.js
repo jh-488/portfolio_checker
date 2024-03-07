0591a1b5cafa0e2f499e3f380124b3af8f1ff474
jh-488 <jihadhyadi01@gmail.com>
2024-03-07 14:43:55 +0100

    updated db infos

--- server/database/Database.js
+++ server/database/Database.js
@@ -2,8 +2,8 @@ const mysql = require("mysql");
 
 
 const db = mysql.createConnection({
-    user: "root",
-    host: "localhost",
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
